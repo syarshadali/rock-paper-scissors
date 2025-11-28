a3d72d521535b066995984cd88a148292b245a0f
Syed Arshad Ali <ssyarshadali@gmail.com>
2025-11-28 11:17:46 +0500

    Create function getComputerChoice in javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+const getComputerChoice = () => {
+    let choice1 = Math.random();
+    if (choice1 < 0.34) {
+        choice1 = 'rock';
+    } else if (choice1 <= 0.67) {
+        choice1 = 'paper';
+    } else {
+        choice1 = 'scissors';
+    }
+    return choice1;
+}
+

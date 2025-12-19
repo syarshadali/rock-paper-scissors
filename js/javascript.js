38993973d3872c589679b104a7afff14b97d4f6d
Syed Arshad Ali <ssyarshadali@gmail.com>
2025-12-19 10:09:03 +0500

    Refactor the code in javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -2,8 +2,17 @@ let humanScore = 0;
 let computerScore = 0;
 
 const getComputerChoice = () => {
-    
+    let choice1 = Math.floor(Math.random() * 3);
+    if (choice1 === 0) {
+        choice1 = "rock";
+    } else if (choice1 === 1) {
+        choice1 = "paper";
+    } else {
+        choice1 = "scissors";
+    }
+    return choice1;
 }
+
 
 const getHumanChoice = () => {
     let choice2 = prompt('choose what to throw:').toLowerCase();
